ecf680e9a0f02117d93a15a7ceb77cb745e033b7
Emily Coleman <86796510+mlecoleman@users.noreply.github.com>
2022-03-26 14:06:49 -0400

    Set counter to have a leading 0 when counter is less than 10 seconds

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-
-
-//Counter
-
-let seconds = 00;
-let outputSeconds = document.getElementById('second');
-let buttonStart = document.getElementById('playbtn');
-let Interval 
-
-buttonStart.addEventListener('click', () => {
-  clearInterval(Interval);
-  Interval = setInterval(startTime, 1000); 
-})
-
-function startTime(){
-  seconds++;
-  outputSeconds.innerHTML = seconds;
-}
- 
-
 //Dropdown window
 const targetDiv = document.getElementById("instructions");
-const btn = document.getElementById("howto");
-btn.onclick = function () {
+const howTo = document.getElementById("howto");
+howTo.onclick = function () {
   if (targetDiv.style.display !== "block") {
     targetDiv.style.display = "block";
   } else {
     targetDiv.style.display = "none";
   }
 };
+
+//Counter
+let seconds = 00;
+let outputSeconds = document.getElementById('second');
+let buttonStart = document.getElementById('playbtn');
+let countInput = document.getElementById('count');
+let timeInput = document.getElementById('time');
+let Interval
+
+buttonStart.addEventListener('click', () => {
+  clearInterval(Interval);
+  Interval = setInterval(startTime, 1000);
+  targetDiv.style.display = "none";
+})
+
+function startTime(){
+  seconds++;
+  outputSeconds.innerHTML = seconds;
+  second.textContent = seconds % 60 > 9 ? seconds % 60 : `0${seconds % 60}`;
+}
+ 
+
+
 
 
 //const instructions = document.querySelector('.play');
